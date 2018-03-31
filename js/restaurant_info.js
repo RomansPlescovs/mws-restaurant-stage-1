7b0e32e71e1a5bdaecf822e143bd06baa31a349e
RomansPlescovs <romans.plescovs@gmail.com>
2018-03-31 12:11:00 +0300

    - moved service worker registration to separate script - cached images - added main role

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -2,6 +2,21 @@ let restaurant;
 var map;
 
 /**
+ * Register service worker
+ */
+if ('serviceWorker' in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    console.log('Service worker registration succeeded:', registration);
+  }).catch(function(error) {
+    console.log('Service worker registration failed:', error);
+  });
+} else {
+  console.log('Service workers are not supported.');
+}
+
+/**
  * Initialize Google map, called from HTML.
  */
 window.initMap = () => {
