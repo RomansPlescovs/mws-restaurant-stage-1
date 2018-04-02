e3332ac43a4518309bf6646fef379327daed8323
RomansPlescovs <romans.plescovs@gmail.com>
2018-04-03 00:41:00 +0300

    moved to dist

---
+++ dist/sw.js
@@ -0,0 +1,61 @@
+const staticCacheName = 'restaurants-v1',
+    filesToCache = [
+        '/index.html',
+        '/restaurant.html',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/js/dbhelper.js',
+        '/sw.js',
+        '/css/styles.css',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
+];
+
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll(filesToCache); 
+        })
+    );
+    console.log("cached files on install");
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurants-') && cacheName !== staticCacheName;
+                }).map(function(cacheName) {
+                    console.log("deleted cache: ", cacheName);
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function(response) {
+                    const requestUrl = new URL(event.request.url);
+                    if (event.request.method!== "POST" && requestUrl.pathname !== "/") {
+                        cache.put(event.request, response.clone());
+                    }
+                    return response;
+                });
+            });
+        })
+    );
+});
